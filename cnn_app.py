57a7554b5b366c595410fd0fb0556a2fed5071ec
Ben chelbi Wissem <Ben_chelbiw@yahoo.com>
2021-01-04 05:57:29 +0100

    cnn

--- cnn_app.py
+++ cnn_app.py
@@ -3,9 +3,9 @@ import keras
 import tensorflow.keras
 from PIL import Image, ImageOps
 import numpy as np
-st.title("Image Classification with Google's Teachable Machine")
-st.header("Brain Tumor MRI Classification Example")
-st.text("Upload a brain MRI Image for image classification as tumor or no-tumor")
+st.title("Image Classification")
+st.header("Brest cancer Classification Example")
+st.text("Upload a brest cancer Image for image classification as cancer or no-cancer")
 
 def teachable_machine_classification(img, weights_file):
     # Load the model
