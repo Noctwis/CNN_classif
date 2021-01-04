b317df2c367430d601d584c240f3390221d40105
Ben chelbi Wissem <Ben_chelbiw@yahoo.com>
2021-01-04 06:12:24 +0100

    cnn

--- cnn_app.py
+++ cnn_app.py
@@ -35,7 +35,7 @@ def teachable_machine_classification(img, weights_file):
 uploaded_file = st.file_uploader("Choose photo ...", type=["jpg", "png"])
 if uploaded_file is not None:
   image = Image.open(uploaded_file)
-  st.image(image, caption='Uploaded MRI.', use_column_width=True)
+  st.image(image, caption='Uploaded cancer.', use_column_width=True)
   st.write("")
   st.write("Classifying...")
   label = teachable_machine_classification(image, 'my_model.hdf5') # Name of the model from Teachablemachine
